94831b3176f5ba4e05a7caf7c8e95177e3fd2e45
Mezue3000 <127846807+Mezue3000@users.noreply.github.com>
2025-01-08 08:49:23 +0100

    Update Blog_post database.sql

--- MySQL Database_Design/Blog_post database.sql
+++ MySQL Database_Design/Blog_post database.sql
@@ -21,8 +21,8 @@ CREATE TABLE IF NOT EXISTS posts(
         post_id    INT PRIMARY KEY AUTO_INCREMENT,
         title      VARCHAR(125) NOT NULL,
         content    VARCHAR(450) NOT NULL,
-	created_at TIMESTAMP DEFAULT(CURTIME()),
-	updated_at TIMESTAMP DEFAULT(CURTIME()),
+	created_at DATETIME NOT NULL DEFAULT NOW(),
+	updated_at DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW(),
         user_id    INT  NOT NULL,
         FOREIGN KEY(user_id)
         REFERENCES users(user_id)
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS posts(
 CREATE TABLE IF NOT EXISTS comments(
 	comment_id   INT PRIMARY KEY AUTO_INCREMENT,
         content      VARCHAR(450) NOT NULL,
-	created_at   TIMESTAMP DEFAULT(CURTIME()),
+	created_at   DATETIME DEFAULT NOW(),
         post_id      INT NOT NULL,
         FOREIGN KEY(post_id)
         REFERENCES posts(post_id)
